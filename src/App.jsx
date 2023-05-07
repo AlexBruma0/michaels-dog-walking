2226198ac327c9aeb9d2481e972e53a0faad11b1
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-05-07 09:21:02 -0700

    page set-up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button,Heading, Box } from '@chakra-ui/react'
+function App() {
+
+  return (
+    <>
+
+          <Heading fontFamily='cursive' bg='inherit' color='white' > MICHAELS DOGWALKING</Heading>
+
+    </>
+  )
+}
+
+export default App
