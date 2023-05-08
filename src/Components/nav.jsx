33fb798522b1ecc121c201f74c3efa22f97ca00a
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-05-08 09:16:04 -0700

    styles

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -5,32 +5,40 @@ const Nav = () => {
     <>
       <Box paddingTop="3%">
         <Button
-          paddingRight="10%"
-          paddingLeft="10%"
+                  colorScheme="messenger"
+                  color='white'
+          paddingRight="6vw"
+          paddingLeft="6vw"
           fontFamily="raleway"
           variant="ghost"
         >
           Services
         </Button>
         <Button
-          paddingRight="10%"
-          paddingLeft="10%"
+                  colorScheme="messenger"
+                  color='white'
+          paddingRight="6vw"
+          paddingLeft="6vw"
           fontFamily="raleway"
           variant="ghost"
         >
           Contact
         </Button>
         <Button
-          paddingRight="10%"
-          paddingLeft="10%"
+          colorScheme="messenger"
+          color='white'
+          paddingRight="6vw"
+          paddingLeft="6vw"
           fontFamily="raleway"
           variant="ghost"
         >
           Location
         </Button>
         <Button
-          paddingRight="10%"
-          paddingLeft="10%"
+                  colorScheme="messenger"
+                  color='white'
+          paddingRight="6vw"
+          paddingLeft="6vw"
           fontFamily="raleway"
           variant="ghost"
         >
